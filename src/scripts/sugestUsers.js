b1329002c26f968be18e0ae946ad08a7a9dcb319
Arthur Meier <Arthurcmeier@outlook.com>
2022-12-04 03:32:01 -0300

    entrega realizada

--- src/scripts/sugestUsers.js
+++ src/scripts/sugestUsers.js
@@ -1,2 +1,18 @@
 //ID dos usuários sugeridos para seguir
-const sugestUsers = [3, 7, 6];
+const sugestUsers = [{
+    id: 3,
+    user: "Carlos Lima",
+    stack: "UX e UI Designer",
+    img: "./src/assets/img/user3.svg",
+}, {
+    id: 7,
+    user: "Samuel Persuhn",
+    stack: "Nao informado",
+    img: "./src/assets/img/user6.svg",
+}, {
+    id: 6,
+    user: "Filipe Gutierry",
+    stack: "Nao informado",
+    img: "./src/assets/img/user7.svg",
+  },
+];
