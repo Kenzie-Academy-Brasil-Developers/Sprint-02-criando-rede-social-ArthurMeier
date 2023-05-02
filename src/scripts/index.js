bbe32da0980b80cec6e7a7e14ec904edb4670b39
Arthur Meier <Arthurcmeier@outlook.com>
2023-05-01 22:25:12 -0300

    Bug Fix

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,322 +1,293 @@
-const perfil = document.getElementById('perfilArea')
-const sugestion = document.getElementById('sugestion')
-const post = document.getElementById('postArea')
+const perfil = document.getElementById("perfilArea");
+const sugestion = document.getElementById("sugestion");
+const post = document.getElementById("postArea");
 
 function criarPerfilArea(user) {
-    const divContainer = document.createElement('div')
-    const imgPerfil = document.createElement('img')
-    const divInfo = document.createElement('div')
-    const name = document.createElement('h2')
-    const stack = document.createElement('p')
-    const divPost = document.createElement('div')
-    const title = document.createElement('input')
-    const text = document.createElement('textarea')
-    const button = document.createElement('button')
+  const divContainer = document.createElement("div");
+  const imgPerfil = document.createElement("img");
+  const divInfo = document.createElement("div");
+  const name = document.createElement("h2");
+  const stack = document.createElement("p");
+  const divPost = document.createElement("div");
+  const title = document.createElement("input");
+  const text = document.createElement("textarea");
+  const button = document.createElement("button");
 
-    perfil.append(divContainer, divPost)
-    divContainer.append(imgPerfil, divInfo)
-    divInfo.append(name, stack)
-    divPost.append(title, text, button)
+  perfil.append(divContainer, divPost);
+  divContainer.append(imgPerfil, divInfo);
+  divInfo.append(name, stack);
+  divPost.append(title, text, button);
 
-    divContainer.classList.add("container__perfil")
-    divInfo.classList.add("container__perfil--info")
-    name.classList.add("perfil__name")
-    stack.classList.add("perfil__stack")
-    divPost.classList.add("container__post")
-    title.classList.add("container__post--title")
-    text.classList.add("container__post--post")
-    button.classList.add("container__post--button")
+  divContainer.classList.add("container__perfil");
+  divInfo.classList.add("container__perfil--info");
+  name.classList.add("perfil__name");
+  stack.classList.add("perfil__stack");
+  divPost.classList.add("container__post");
+  title.classList.add("container__post--title");
+  text.classList.add("container__post--post");
+  button.classList.add("container__post--button");
 
-    imgPerfil.src = user.img
-    imgPerfil.alt = "Foto de perfil"
-    name.innerText = user.user
-    name.id = "user"
-    stack.innerText = user.stack
-    title.placeholder = "    Digitar título do post"
-    text.placeholder = "   Digitar descrição do post"
-    button.innerText = "Postar"
-    button.setAttribute('type', 'submit')
-    button.id = "postar"
+  imgPerfil.src = user.img;
+  imgPerfil.alt = "Foto de perfil";
+  name.innerText = user.user;
+  name.id = "user";
+  stack.innerText = user.stack;
+  title.placeholder = "    Digitar título do post";
+  text.placeholder = "   Digitar descrição do post";
+  button.innerText = "Postar";
+  button.setAttribute("type", "submit");
+  button.id = "postar";
 }
 
 function criarAside(array) {
-    const title = document.createElement('h2')
-    const lista = document.createElement('ul')
+  const title = document.createElement("h2");
+  const lista = document.createElement("ul");
 
-    sugestion.append(title, lista)
+  sugestion.append(title, lista);
 
-    title.innerText = 'Sugestões para você seguir'
+  title.innerText = "Sugestões para você seguir";
 
-    for (let i = 0; i < array.length; i++) {
-        const item = document.createElement('li')
-        const divPerfil = document.createElement('div')
-        const img = document.createElement('img')
-        const divInfo = document.createElement('div')
-        const name = document.createElement('h2')
-        const stack = document.createElement('p')
-        const button = document.createElement('button')
+  for (let i = 0; i < array.length; i++) {
+    const item = document.createElement("li");
+    const divPerfil = document.createElement("div");
+    const img = document.createElement("img");
+    const divInfo = document.createElement("div");
+    const name = document.createElement("h2");
+    const stack = document.createElement("p");
+    const button = document.createElement("button");
 
-        lista.appendChild(item)
-        item.append(divPerfil, button)
-        divPerfil.append(img, divInfo)
-        divInfo.append(name, stack)
+    lista.appendChild(item);
+    item.append(divPerfil, button);
+    divPerfil.append(img, divInfo);
+    divInfo.append(name, stack);
 
-        item.classList.add('container__sugestoes')
-        divPerfil.classList.add('container__perfil')
-        divInfo.classList.add('container__perfil--info')
-        name.classList.add('perfil__name')
-        stack.classList.add('perfil__stack')
-        button.classList.add('seguir')
+    item.classList.add("container__sugestoes");
+    divPerfil.classList.add("container__perfil");
+    divInfo.classList.add("container__perfil--info");
+    name.classList.add("perfil__name");
+    stack.classList.add("perfil__stack");
+    button.classList.add("seguir");
 
-        img.src = array[i].img
-        img.alt = "Foto de perfil"
-        name.innerText = array[i].user
-        stack.innerText = array[i].stack
-        button.innerText = "Seguir"
-        button.dataset.seguir = "seguir"
-    }
-
-
+    img.src = array[i].img;
+    img.alt = "Foto de perfil";
+    name.innerText = array[i].user;
+    stack.innerText = array[i].stack;
+    button.innerText = "Seguir";
+    button.dataset.seguir = "seguir";
+  }
 }
 
 function criarPostArea(array) {
-    const title = document.createElement('h1')
-    const lista = document.createElement('ul')
+  const title = document.createElement("h1");
+  const lista = document.createElement("ul");
 
-    lista.classList.add('postListArea')
+  lista.classList.add("postListArea");
 
-    title.innerText = "Posts"
+  title.innerText = "Posts";
 
-    post.append(title, lista)
+  post.append(title, lista);
 
-    for (let i = 0; i < array.length; i++) {
-        for (let e = 0; e < users.length; e++) {
-            if (users[e].id == array[i].user) {
-                const item = document.createElement('li')
-                const divPerfil = document.createElement('div')
-                const imgPerfil = document.createElement('img')
-                const divInfo = document.createElement('div')
-                const name = document.createElement('h2')
-                const stack = document.createElement('p')
-                const divPost = document.createElement('div')
-                const titlePost = document.createElement('h1')
-                const text = document.createElement('p')
-                const divButton = document.createElement('div')
-                const button = document.createElement('button')
-                const heart = document.createElement('i')
-                const likeCount = document.createElement('span')
+  for (let i = 0; i < array.length; i++) {
+    for (let e = 0; e < users.length; e++) {
+      if (users[e].id == array[i].user) {
+        const item = document.createElement("li");
+        const divPerfil = document.createElement("div");
+        const imgPerfil = document.createElement("img");
+        const divInfo = document.createElement("div");
+        const name = document.createElement("h2");
+        const stack = document.createElement("p");
+        const divPost = document.createElement("div");
+        const titlePost = document.createElement("h1");
+        const text = document.createElement("p");
+        const divButton = document.createElement("div");
+        const button = document.createElement("button");
+        const heart = document.createElement("i");
+        const likeCount = document.createElement("span");
 
-                lista.appendChild(item)
-                item.append(divPerfil, divPost)
-                divPerfil.append(imgPerfil, divInfo)
-                divInfo.append(name, stack)
-                divPost.append(titlePost, text, divButton)
-                divButton.append(button, heart, likeCount)
+        lista.appendChild(item);
+        item.append(divPerfil, divPost);
+        divPerfil.append(imgPerfil, divInfo);
+        divInfo.append(name, stack);
+        divPost.append(titlePost, text, divButton);
+        divButton.append(button, heart, likeCount);
 
-                divPerfil.classList.add('container__perfil')
-                divInfo.classList.add('container__perfil--info')
-                name.classList.add('perfil__name')
-                stack.classList.add('perfil__stack')
-                divPost.classList.add('container__posted')
-                divButton.classList.add('container__posted--buttons')
-                button.classList.add('container__posted--button')
-                heart.classList.add('fa-solid', 'fa-heart')
+        divPerfil.classList.add("container__perfil");
+        divInfo.classList.add("container__perfil--info");
+        name.classList.add("perfil__name");
+        stack.classList.add("perfil__stack");
+        divPost.classList.add("container__posted");
+        divButton.classList.add("container__posted--buttons");
+        button.classList.add("container__posted--button");
+        heart.classList.add("fa-solid", "fa-heart");
 
-                imgPerfil.src = users[e].img
-                imgPerfil.alt = "Foto de perfil"
-                name.innerText = users[e].user
-                stack.innerText = users[e].stack
-                titlePost.innerText = array[i].title
-                text.innerText = array[i].text
-                button.innerText = "Abrir Post"
-                button.dataset.id = array[i].id_post
-                likeCount.innerText = Number(25)
-                likeCount.dataset.likeCount = array[i].id_post
-                heart.dataset.like = array[i].id_post
-
-            }
-
-        }
+        imgPerfil.src = users[e].img;
+        imgPerfil.alt = "Foto de perfil";
+        name.innerText = users[e].user;
+        stack.innerText = users[e].stack;
+        titlePost.innerText = array[i].title;
+        text.innerText = array[i].text;
+        button.innerText = "Abrir Post";
+        button.dataset.id = array[i].id_post;
+        likeCount.innerText = Number(25);
+        likeCount.dataset.likeCount = array[i].id_post;
+        heart.dataset.like = array[i].id_post;
+      }
     }
-
+  }
 }
 
 function renderSite() {
-    criarPerfilArea(users[1])
-    criarAside(sugestUsers)
-    criarPostArea(posts)
-    seguir()
-    like()
-    renderModal()
-    postar()
+  criarPerfilArea(users[1]);
+  criarAside(sugestUsers);
+  criarPostArea(posts);
+  seguir();
+  like();
+  renderModal();
+  postar();
 }
 
 function renderModal() {
-    const modal = document.querySelector('.modal__container')
-    const buttons = document.querySelectorAll('.container__posted--button')
+  const modal = document.querySelector(".modal__container");
+  const buttons = document.querySelectorAll(".container__posted--button");
 
-    for (let i = 0; i < buttons.length; i++) {
-        const button = buttons[i]
+  for (let i = 0; i < buttons.length; i++) {
+    const button = buttons[i];
 
-        button.addEventListener('click', () => {
-            const modalContent = createModal(button.dataset.id)
+    button.addEventListener("click", () => {
+      const modalContent = createModal(button.dataset.id);
 
-            modal.innerHTML = ''
+      modal.innerHTML = "";
 
-            modal.appendChild(modalContent)
+      modal.appendChild(modalContent);
 
-            modal.showModal()
+      modal.showModal();
 
-            closeModal()
-
-        })
-    }
+      closeModal();
+    });
+  }
 }
 
 function createModal(id) {
-    const modalContainer = document.createElement('div')
-    const modalDivPerfil = document.createElement('div')
-    const modalImg = document.createElement('img')
-    const modalDivInfo = document.createElement('div')
-    const modalName = document.createElement('h2')
-    const modalStack = document.createElement('p')
-    const modalTitle = document.createElement('h1')
-    const modalText = document.createElement('p')
-    const modalClose = document.createElement('p')
-    let element = {}
+  const modalContainer = document.createElement("div");
+  const modalDivPerfil = document.createElement("div");
+  const modalImg = document.createElement("img");
+  const modalDivInfo = document.createElement("div");
+  const modalName = document.createElement("h2");
+  const modalStack = document.createElement("p");
+  const modalTitle = document.createElement("h1");
+  const modalText = document.createElement("p");
+  const modalClose = document.createElement("p");
+  let element = {};
 
-    console.log(id);
+  console.log(id);
 
-    for (let i = 0; i < posts.length; i++) {
-        if (posts[i].id_post == Number(id)) {
-            element = posts[i]
-        }
+  for (let i = 0; i < posts.length; i++) {
+    if (posts[i].id_post == Number(id)) {
+      element = posts[i];
     }
+  }
 
-    for (let e = 0; e < users.length; e++) {
-        if (users[e].id == element.user) {
+  for (let e = 0; e < users.length; e++) {
+    if (users[e].id == element.user) {
+      modalImg.src = users[e].img;
+      modalImg.alt = "Foto de perfil";
+      modalName.innerText = users[e].user;
+      modalStack.innerText = users[e].stack;
+      modalTitle.innerText = element.title;
+      modalText.innerText = element.text;
+      modalClose.innerText = "X";
 
-            modalImg.src = users[e].img
-            modalImg.alt = "Foto de perfil"
-            modalName.innerText = users[e].user
-            modalStack.innerText = users[e].stack
-            modalTitle.innerText = element.title
-            modalText.innerText = element.text
-            modalClose.innerText = 'X'
+      modalDivPerfil.classList.add("container__perfil");
+      modalDivInfo.classList.add("container__perfil--info");
+      modalName.classList.add("perfil__name");
+      modalStack.classList.add("perfil__stack");
+      modalText.classList.add("modal__text");
+      modalClose.classList.add("modal__close");
 
-            modalDivPerfil.classList.add('container__perfil')
-            modalDivInfo.classList.add('container__perfil--info')
-            modalName.classList.add('perfil__name')
-            modalStack.classList.add('perfil__stack')
-            modalText.classList.add('modal__text')
-            modalClose.classList.add('modal__close')
+      modalContainer.append(modalDivPerfil, modalTitle, modalText, modalClose);
+      modalDivPerfil.append(modalImg, modalDivInfo);
+      modalDivInfo.append(modalName, modalStack);
 
-
-            modalContainer.append(modalDivPerfil, modalTitle, modalText, modalClose)
-            modalDivPerfil.append(modalImg, modalDivInfo)
-            modalDivInfo.append(modalName, modalStack)
-
-            return modalContainer
-        }
+      return modalContainer;
     }
-
+  }
 }
 
 function closeModal() {
-    const modal = document.querySelector('.modal__container')
-    const closeBtn = document.querySelector('.modal__close')
+  const modal = document.querySelector(".modal__container");
+  const closeBtn = document.querySelector(".modal__close");
 
-    closeBtn.addEventListener('click', () => {
-        modal.close()
-    })
+  closeBtn.addEventListener("click", () => {
+    modal.close();
+  });
 }
 
 function seguir() {
-    const buttonToggle = document.querySelectorAll("[data-seguir]")
+  const buttonToggle = document.querySelectorAll("[data-seguir]");
 
-    for (let i = 0; i < buttonToggle.length; i++) {
+  for (let i = 0; i < buttonToggle.length; i++) {
+    buttonToggle[i].addEventListener("click", () => {
+      buttonToggle[i].classList.toggle("seguir");
+      buttonToggle[i].classList.toggle("seguindo");
 
-        buttonToggle[i].addEventListener('click', () => {
-
-            buttonToggle[i].classList.toggle('seguir')
-            buttonToggle[i].classList.toggle('seguindo')
-
-            if (buttonToggle[i].classList == "seguindo") {
-
-                buttonToggle[i].innerText = "Seguindo"
-
-            }
-            else {
-
-                buttonToggle[i].innerText = "Seguir"
-
-            }
-
-        })
-
-    }
+      if (buttonToggle[i].classList == "seguindo") {
+        buttonToggle[i].innerText = "Seguindo";
+      } else {
+        buttonToggle[i].innerText = "Seguir";
+      }
+    });
+  }
 }
 
 function like() {
-    const heart = document.querySelectorAll("[data-like]")
-    const likeCount = document.querySelectorAll('[data-like-count]')
+  const heart = document.querySelectorAll("[data-like]");
+  const likeCount = document.querySelectorAll("[data-like-count]");
 
-    for (let i = 0; i < heart.length; i++) {
+  for (let i = 0; i < heart.length; i++) {
+    heart[i].addEventListener("click", () => {
+      heart[i].classList.toggle("like");
 
-        heart[i].addEventListener('click', () => {
-
-            heart[i].classList.toggle('like')
-
-            for (let e = 0; e < likeCount.length; e++) {
-                console.log(likeCount[e]);
-                if (heart[i].classList == "fa-solid fa-heart like") {
-                    likeCount[e].innerText = 26
-                }
-                else {
-                    likeCount[e].innerText = 25
-                }
-
-            }
-            console.log(likeCount);
-
-        })
-    }
+      for (let e = 0; e < likeCount.length; e++) {
+        console.log(likeCount[e]);
+        if (heart[i].classList == "fa-solid fa-heart like") {
+          likeCount[e].innerText = 26;
+        } else {
+          likeCount[e].innerText = 25;
+        }
+      }
+      console.log(likeCount);
+    });
+  }
 }
 
 function postar() {
-    const button = document.querySelector('#postar')
-    const postTitle = document.querySelector('.container__post--title')
-    const postText = document.querySelector('.container__post--post')
-    const postPerfil = document.querySelector('#user')
-    const perfilArea = document.querySelector('#perfilArea')
+  const button = document.querySelector("#postar");
+  const postTitle = document.querySelector(".container__post--title");
+  const postText = document.querySelector(".container__post--post");
+  const postPerfil = document.querySelector("#user");
+  const perfilArea = document.querySelector("#perfilArea");
 
-    button.addEventListener('click', () => {
+  button.addEventListener("click", () => {
+    for (let i = 0; i < users.length; i++) {
+      if (postPerfil.textContent == users[i].user) {
+        posts.push({
+          id_post: posts.length + 2,
+          user: users[i].id,
+          title: postTitle.value,
+          text: postText.value,
+        });
 
-        for (let i = 0; i < users.length; i++) {
+        post.innerHTML = "";
+        perfilArea.innerHTML = "";
 
-            if (postPerfil.textContent == users[i].user) {
-
-                posts.push({
-                    id_post: posts.length + 2,
-                    user: users[i].id,
-                    title: postTitle.value,
-                    text: postText.value
-                })
-
-                post.innerHTML = ''
-                perfilArea.innerHTML = ''
-
-                criarPostArea(posts)
-                like()
-                renderModal()
-                criarPerfilArea(users[1])
-                postar()
-
-            }
-
-        }
-    })
-
+        criarPostArea(posts);
+        like();
+        renderModal();
+        criarPerfilArea(users[1]);
+        postar();
+      }
+    }
+  });
 }
 
-renderSite()
+renderSite();
--- src/scripts/sugestUsers.js
+++ src/scripts/sugestUsers.js
@@ -1,15 +1,18 @@
 //ID dos usuários sugeridos para seguir
-const sugestUsers = [{
+const sugestUsers = [
+  {
     id: 3,
     user: "Carlos Lima",
     stack: "UX e UI Designer",
     img: "./src/assets/img/user3.svg",
-}, {
+  },
+  {
     id: 7,
     user: "Samuel Persuhn",
     stack: "Nao informado",
     img: "./src/assets/img/user2.svg",
-}, {
+  },
+  {
     id: 6,
     user: "Filipe Gutierry",
     stack: "Nao informado",
